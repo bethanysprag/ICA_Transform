688ebb76739a65e1bc5ddd81971226d3510ff53b
Bethany Sprague <bethanysprag@gmail.com>
2022-11-20 17:39:05 +0000

    Issue #2 - creating cf templates

---
+++ scripts/ecr_create.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws cloudformation deploy --template-file cf/ecr_template.yml --stack-name ecr-ica --no-fail-on-empty-changeset
---
+++ scripts/ecs_create.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+aws cloudformation deploy --template-file cf/ecs_template.yml --stack-name ecs-ica --no-fail-on-empty-changeset
+

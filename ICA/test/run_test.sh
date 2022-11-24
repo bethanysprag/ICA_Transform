44d52bb9bbf199033b62bdfc6cc9f254e5646270
Bethany Sprague <bethanysprag@gmail.com>
2022-11-24 04:09:39 +0000

    troubleshooting dockerfile test error. Doesnt seem to be passing AWS_DEFAULT REGION

--- ICA/test/run_test.sh
+++ ICA/test/run_test.sh
@@ -2,6 +2,8 @@
 set -e 
 if [ "$local" = 'True' ]; then
   echo 'configuring aws using local keys'
+  echo 'AWS_ACCOUNT_ID' && echo 'AWS_DEFAULT_REGION'
+  echo $AWS_ACCOUNT_ID && echo $AWS_DEFAULT_REGION
   aws configure set aws_access_key_id $AWS_ACCESS_KEY && aws configure set aws_secret_access_key $AWS_SECRET_KEY && aws configure set region $AWS_DEFAULT_REGION
 else
   echo 'running test in ecs container mode'
